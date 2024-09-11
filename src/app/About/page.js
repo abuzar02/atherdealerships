11ed8eea261a57aaacdb041ae7b3666936f25bc9
abuzar02 <mdabujaralam3@gmail.com>
2024-09-11 22:14:12 +0530

    doneee

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import Image from 'next/image'
+
 import "../style/about.css"
-import aboutcool from "../component/images/about-main.jpg"
+
 
 
 const page = () => {
   return (
     <>
       <div className='ab-main'>
-        <Image src={aboutcool} alt='not support' />
+       
         <div className="pick-content">
           <h1>About Us</h1>
         </div>
